35f9960915cd43f26fce236f71afaec7bcfb5ee8
niteshrathore123 <rathoreniks53@gmail.com>
2020-06-15 09:54:00 +0530

    intial commit

---
+++ app/src/main/java/com/nitesh/contactsyncproject/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.nitesh.contactsyncproject
+
+import android.R.layout
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.SignInButton
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+
+}
---
+++ app/src/main/java/com/nitesh/contactsyncproject/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.nitesh.contactsyncproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    lateinit var handler:Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        handler= Handler()
+        handler.postDelayed({
+            val intent= Intent (this@MainActivity,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
